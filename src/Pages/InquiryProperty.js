ff18a8b98e00babcb985b2823b977044647a23ab
sragu2000 <s.s.raguraj@gmail.com>
2022-08-27 18:02:09 +0530

    chat

--- src/Pages/InquiryProperty.js
+++ src/Pages/InquiryProperty.js
@@ -9,13 +9,26 @@ function InquiryProperty() {
             <NavBar></NavBar>
             <div className="container mt-5">
                 <form>
-                    <div></div>
-                    <div className="row">
+                    <div style={{ "height": "70vh" }} className="border border-dark rounded">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div id="received">
+                                    <div className="alert alert-danger" style={{"width":"50vw"}}>Hello</div>
+                                </div>
+                            </div>
+                            <div className="col-md-12">
+                                <div id="sent">
+                                    <div className="alert alert-success" style={{"width":"50vw"}} align="right">Hello</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-2">
                         <div className="col-md-10">
                             <input className="form-control" placeholder="Message"></input>
                         </div>
                         <div className="col-md-2">
-                            <button type="submit" className="btn btn-outline-primary form-control">
+                            <button type="submit" className="btn btn-outline-primary form-control ">
                                 <FontAwesomeIcon icon={faPaperPlane} /> &nbsp;
                                 Send
                             </button>
